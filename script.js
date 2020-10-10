39c9c96263b6e61254b871f704cdd3a6d7c4cb6f
jandeh7 <andehjane@gmail.com>
2020-10-10 17:50:44 +0100

    old changes

--- script.js
+++ script.js
@@ -7,19 +7,24 @@ const navbar = document.querySelector(".navbar");
 
 const chat_box = document.getElementById("chatbox");
 
-chat_box.style.display = "none";
+// chat_box.style.display = "none";
 
-const chat_box_button = document.getElementById("chatBoxButton");
- 
+const chatboxButton = document.getElementById("chatBoxButton");
+const logInPage = document.getElementById("log-page");
+const signUpPage = document.getElementById("sign-page");
+const logInTab = document.querySelector("#login-tab");
+const signUpTab = document.querySelector("#signup-tab");
+
+
 /** Toggle mobile menu **/
 function toggleMenu() {
-    if (navbar.classList.contains("active")) {
-        navbar.classList.remove("active");
+    if (navbar.classList.contains("show")) {
+        navbar.classList.remove("show");
          
         // adds the menu (hamburger) icon
         toggle.querySelector("a").innerHTML = "<i class=\"fas fa-bars\"></i>";
     } else {
-        navbar.classList.add("active");
+        navbar.classList.add("show");
          
         // adds the close (x) icon
         toggle.querySelector("a").innerHTML = "<i class=\"fas fa-times\"></i>";
@@ -115,9 +120,6 @@ function answers7(){
     }
 }
 
-/** Event Listener **/
-toggle.addEventListener("click", toggleMenu, false);
-
 function toggleChatBox(e){
 
     e.preventDefault();
@@ -134,6 +136,30 @@ function toggleChatBox(e){
     
 }
 
-chat_box_button.addEventListener("click", toggleChatBox, false);
+chatboxButton.addEventListener("click", toggleChatBox, false);
 
-//toggleChatBox();
+// toggleChatBox();
+
+// LOGIN AND SIGN UP TAB
+
+function tabForm(param) {
+    if(param === 'signup'){
+        logInTab.style.display = "none";
+        signUpTab.style.display = "block";
+        signUpPage.classList.add("current");
+        logInPage.classList.remove("current");
+    } else {
+        logInTab.style.display = "block";
+        signUpTab.style.display = "none";
+        logInPage.classList.add("current");
+        signUpPage.classList.remove("current");
+    }
+}
+
+function reveal(x){
+    if(document.getElementById(x).style.display === "none"){
+        document.getElementById(x).style.display = "block";
+    } else {
+        document.getElementById(x).style.display = "none";
+    }
+}
