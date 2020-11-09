4c66dfb7badcd38aa1d8b3277e624d2bddc7599a
jandeh7 <andehjane@gmail.com>
2020-11-09 08:37:45 +0100

    commit ui change

--- dashboard/assets/js/common/users.js
+++ dashboard/assets/js/common/users.js
@@ -83,17 +83,18 @@ function validateLogin(userName, upassword) {
 }
 
 //USER LOGIN
-function userLogin(event) {
-    (userName = document.getElementById("_username").value).toString;
-    upassword = document.getElementById("_password").value;
-    rememberMe = document.getElementById("rememberMe").value;
+function userLogin() {
+    userName = document.getElementById("_username").value;
+    upassword = document.getElementById("_pass").value;
+    rememberMe = document.getElementById("_rememberMe").checked;
+  
     validateUser = validateLogin(userName, upassword);
     if(validateUser == true) {
-        /*  if(rememberMe == true) {
+     /*  if(rememberMe == true) {
           setCookie(userName, false);
          } else {
           setCookie(userName, true);
-         } */  
+         }  */
         loggedUserIn(userName);
         localStorage.setItem("logged", JSON.stringify(userName.toLowerCase()));
         window.location.assign("dashboard/index.html");
@@ -102,12 +103,13 @@ function userLogin(event) {
         document.getElementById("error-login").style.display = "Block";
         document.getElementById("error-login").innerHTML = "Invalid Username/Password!!";
         
-    }
-    event.preventDefault();
+    } 
+    event.preventDefault(); 
 }
 
 //ADD NEW USER TO THE DATABASE (LOCAL STORAGE)
-function addUser(event) {
+function addUser() {
+  
   users = getUsers();
   usersIdTracker = parseInt(getUserIdTracker()) + 1;
   validateUserSignup = validateSignup();
@@ -135,11 +137,11 @@ function addUser(event) {
         loggedUserIn(username);
         localStorage.setItem("logged", JSON.stringify(newUser.userName));
         localStorage.setItem("usersIdTracker", usersIdTracker);
-        window.location.assign("./welcome.html");
+        window.location.assign("welcome.html");
     } else {
       document.getElementById("error-signup").style.display = "Block";
       document.getElementById("error-signup").innerHTML = validateUserSignup;
-      window.location.assign("#formSignUp");
+      //window.location.assign("#formSignUp");
     }
     event.preventDefault();
 }
@@ -150,163 +152,255 @@ users = getUsers();
 loggedIn = isLoggedIn();
 
 if (loggedIn == false) {
-  window.location.assign("login.html");
+  window.location.assign("index.html");
 } else {
     userIndex = getUserIndex(loggedIn);
+    userLocation = document.getElementById("userLocation").value;
+        switch (userLocation) {
+          case "Abia":
+            latlong = [5.532003041,7.486002487];
+            break;
+          case "Adamawa":
+            latlong = [10.2703408,13.2700321];
+            break;
+          case "Akwa Ibom":
+            latlong = [5.007996056,7.849998524];
+            break;
+          case "Anambra":
+            latlong = [6.210433572,7.06999711];
+            break;
+          case "Bauchi":
+            latlong = [11.68040977,10.19001339];
+            break;
+          case "Bayelsa":
+            latlong = [4.664030,6.036987];
+            break;
+          case "Benue":
+            latlong = [7.190399596,8.129984089];
+            break;
+          case "Borno":
+            latlong = [10.62042279,12.18999467];
+            break;
+          case "Cross River":
+            latlong = [4.960406513,8.330023558];
+            break;
+          case "Delta":
+            latlong = [5.890427265,5.680004434];
+            break;
+          case "Edo":
+            latlong = [6.340477314,5.620008096];
+            break;
+          case "Ekiti":
+            latlong = [7.630372741,5.219980834];
+            break;
+          case "Enugu":
+            latlong = [6.867034321,7.383362995];
+            break;
+          case "Abuja":
+            latlong = [9.083333149,7.533328002];
+            break;
+          case "Gombe":
+            latlong = [10.29044293,11.16995357];
+            break;
+          case "Imo":
+            latlong = [5.492997053,7.026003588];
+            break;
+          case "Jigawa":
+            latlong = [11.7991891,9.350334607];
+            break;
+          case "Kaduna":
+            latlong = [11.0799813,7.710009724];
+            break;
+          case "Kano":
+            latlong = [11.99997683,8.5200378];
+            break;
+          case "Katsina":
+            latlong = [11.5203937,7.320007689];
+            break;
+          case "Kebbi":
+            latlong = [12.45041445,4.199939737];
+            break;
+          case "Kogi":
+            latlong = [7.800388203,6.739939737];
+            break;
+          case "Kwara":
+            latlong = [8.490010192,4.549995889];
+            break;
+          case "Lagos":
+            latlong = [6.443261653,3.391531071];
+            break;
+          case "Nasarawa":
+            latlong = [8.490423603,8.5200378];
+            break;
+          case "Niger":
+            latlong = [10.4003587,5.469939737];
+            break;
+          case "Ogun":
+            latlong = [7.160427265,3.350017455];
+            break;
+          case "Ondo":
+            latlong = [7.250395934,5.199982054];
+            break;
+          case "Osun":
+            latlong = [7.629959329,4.179992634];
+            break;
+          case "Oyo":
+            latlong = [7.970016092,3.590002806];
+            break;
+          case "Plateau":
+            latlong = [9.929973978,8.890041055];
+            break;
+          case "Rivers":
+            latlong = [4.810002257,7.010000772];
+            break;
+          case "Sokoto":
+            latlong = [13.06001548,5.240031289];
+            break;
+          case "Taraba":
+            latlong = [7.870409769,9.780012572];
+            break;
+          case "Yobe":
+            latlong = [11.74899608,11.96600457];
+            break;
+          case "Zamfara":
+            latlong = [12.1704057,6.659996296];
+            break;
+          default:
+            latlong = [];
+      }
     sex = document.getElementById("sex").value;
     phone = document.getElementById("phone").value;
-    userLocation = document.getElementById("userLocation").value;
-    switch (userLocation) {
-      case "Abia":
-        latlong = [5.532003041,7.486002487];
-        break;
-      case "Adamawa":
-        latlong = [10.2703408,13.2700321];
-        break;
-      case "Akwa Ibom":
-        latlong = [5.007996056,7.849998524];
-        break;
-      case "Anambra":
-        latlong = [6.210433572,7.06999711];
-        break;
-      case "Bauchi":
-        latlong = [11.68040977,10.19001339];
-        break;
-      case "Bayelsa":
-        latlong = [4.664030,6.036987];
-        break;
-      case "Benue":
-        latlong = [7.190399596,8.129984089];
-        break;
-      case "Borno":
-        latlong = [10.62042279,12.18999467];
-        break;
-      case "Cross River":
-        latlong = [4.960406513,8.330023558];
-        break;
-      case "Delta":
-        latlong = [5.890427265,5.680004434];
-        break;
-      case "Edo":
-        latlong = [6.340477314,5.620008096];
-        break;
-      case "Ekiti":
-        latlong = [7.630372741,5.219980834];
-        break;
-      case "Enugu":
-        latlong = [6.867034321,7.383362995];
-        break;
-      case "Abuja":
-        latlong = [9.083333149,7.533328002];
-        break;
-      case "Gombe":
-        latlong = [10.29044293,11.16995357];
-        break;
-      case "Imo":
-        latlong = [5.492997053,7.026003588];
-        break;
-      case "Jigawa":
-        latlong = [11.7991891,9.350334607];
-        break;
-      case "Kaduna":
-        latlong = [11.0799813,7.710009724];
-        break;
-      case "Kano":
-        latlong = [11.99997683,8.5200378];
-        break;
-      case "Katsina":
-        latlong = [11.5203937,7.320007689];
-        break;
-      case "Kebbi":
-        latlong = [12.45041445,4.199939737];
-        break;
-      case "Kogi":
-        latlong = [7.800388203,6.739939737];
-        break;
-      case "Kwara":
-        latlong = [8.490010192,4.549995889];
-        break;
-      case "Lagos":
-        latlong = [6.443261653,3.391531071];
-        break;
-      case "Nasarawa":
-        latlong = [8.490423603,8.5200378];
-        break;
-      case "Niger":
-        latlong = [10.4003587,5.469939737];
-        break;
-      case "Ogun":
-        latlong = [7.160427265,3.350017455];
-        break;
-      case "Ondo":
-        latlong = [7.250395934,5.199982054];
-        break;
-      case "Osun":
-        latlong = [7.629959329,4.179992634];
-        break;
-      case "Oyo":
-        latlong = [7.970016092,3.590002806];
-        break;
-      case "Plateau":
-        latlong = [9.929973978,8.890041055];
-        break;
-      case "Rivers":
-        latlong = [4.810002257,7.010000772];
-        break;
-      case "Sokoto":
-        latlong = [13.06001548,5.240031289];
-        break;
-      case "Taraba":
-        latlong = [7.870409769,9.780012572];
-        break;
-      case "Yobe":
-        latlong = [11.74899608,11.96600457];
-        break;
-      case "Zamfara":
-        latlong = [12.1704057,6.659996296];
-        break;
-      default:
-        latlong = [];
-    } 
-
-    if(sex == "male") {
-         img = "https://firebasestorage.googleapis.com/v0/b/i2talk.appspot.com/o/images%2Fmale.png?alt=media&token=48011726-146d-4581-b91f-9c58fb4fda4b";
-    } else if(sex == "female") {
-         img = "https://firebasestorage.googleapis.com/v0/b/i2talk.appspot.com/o/images%2Ffemale.png?alt=media&token=74527855-9a7e-4950-9819-f2faa87f82fa";
+    testReg = validateComp(phone, sex, userLocation);
+    if (testReg == true) {
+        if(sex == "male") {
+              img = "https://firebasestorage.googleapis.com/v0/b/i2talk.appspot.com/o/images%2Fmale.png?alt=media&token=48011726-146d-4581-b91f-9c58fb4fda4b";
+        } else if(sex == "female") {
+              img = "https://firebasestorage.googleapis.com/v0/b/i2talk.appspot.com/o/images%2Ffemale.png?alt=media&token=74527855-9a7e-4950-9819-f2faa87f82fa";
+        } else {
+            img = "https://firebasestorage.googleapis.com/v0/b/i2talk.appspot.com/o/images%2Fdefault.png?alt=media&token=3f7238ec-d4c9-43f3-bdb7-9a3f323b12ea";
+        }
+        tempUsers = JSON.parse(localStorage.getItem("tempUsers"))
+        tempUsers.img = img;
+        tempUsers.phone = phone;
+        tempUsers.location = userLocation;
+        tempUsers.sex = sex;
+        tempUsers.latlong=latlong;
+        // alert(JSON.stringify(tempUsers))
+        db.collection("users").add(tempUsers).then((ref) => {
+          // console.log("Added User with ID:", ref.id)
+          swal({
+            icon: "success",
+            title: "Registration Successful",
+            button: {
+              text: "Proceed to Login",
+              value: true,
+              visible: true,
+              className: "",
+              closeModal: true,
+            },
+            // .swal-modal {
+            //   background-color: rgba(63,255,106,0.69);
+            //   border: 3px solid white;
+            // }
+          }).then(() => {
+            localStorage.removeItem("tempUsers");
+            setTimeout(function(){window.location.assign("index.html")}, 1000);
+          });
+          // setTimeout(function(){ window.location.assign("dashboard/index.html") }, 2000);
+        })
+        
     } else {
-       img = "https://firebasestorage.googleapis.com/v0/b/i2talk.appspot.com/o/images%2Fdefault.png?alt=media&token=3f7238ec-d4c9-43f3-bdb7-9a3f323b12ea";
+      document.getElementById("error-welcome").style.display = "Block";
+      document.getElementById("error-welcome").innerHTML = testReg;
+
     }
-    tempUsers = JSON.parse(localStorage.getItem("tempUsers"))
-    tempUsers.img = img;
-    tempUsers.phone = phone;
-    tempUsers.location = userLocation;
-    tempUsers.sex = sex;
-    tempUsers.latlong=latlong;
-    // alert(JSON.stringify(tempUsers))
-    db.collection("users").add(tempUsers).then((ref) => {
-      // console.log("Added User with ID:", ref.id)
-      swal({
-        icon: "success",
-        title: "Registration Successful",
-        button: {
-          text: "Proceed to Login",
-          value: true,
-          visible: true,
-          className: "",
-          closeModal: true,
-        },
-        // .swal-modal {
-        //   background-color: rgba(63,255,106,0.69);
-        //   border: 3px solid white;
-        // }
-      }).then(() => {
-        localStorage.removeItem("tempUsers");
-        setTimeout(function(){window.location.assign("login.html")}, 1000);
-      });
-      // setTimeout(function(){ window.location.assign("dashboard/index.html") }, 2000);
-    })
+
+    
   }
   event.preventDefault();
+}
+
+//VALIDATE COMPLETE REGISTRATION
+function validateComp(phone, sex, userLocation) {
+    if (userLocation == "none") {
+      return "Error: Select Location";
+    }
+    else if(sex == "none") {
+      return "Error: Select Sex";
+    }
+    else if (validatePhone(phone) != true) {
+      return "Error: Invalid Phone Number"
+    } else {
+      return true;
+    }
+}
+
+function validatePhone(phone) {
+  var telcoPrefixes = [703, 706, 803, 806, 810, 813, 814, 816, 903, 705, 805, 811, 815, 905, 701, 708, 802, 808, 812, 902, 809, 817, 818, 909, 804];
+
+  //search array for specific values
+  function in_array(value, array){
+    var index = array.indexOf(value);
+    if(index == -1){
+      return false;
+    }else{
+      return index;
+    }
+  }
+
+  if (phone.length == 11) {
+    mobilePrefix = Number(phone.substr(1,3));
+    firstFigure = Number(phone[0]);
+
+    //check if mobile prefix exists in telcoPrefixes array
+    checkArray = in_array(mobilePrefix, telcoPrefixes);
+    if(checkArray === false){
+      return false;
+    } else if(checkArray > 0 && firstFigure === 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  else if (phone.length == 13) {
+    //get mobile number prefix - 706 or 703 - depending on telco
+    mobilePrefix = Number(phoneInputValue.substr(3,3));
+    //get dialling code from mobile number
+    dialingCode = Number(phoneInputValue.substr(0,3));
+
+    //check if mobile prefix exists in telcoPrefixes array		
+    checkArray = in_array(mobilePrefix, telcoPrefixes);
+    if(checkArray === false){
+      return false;
+    } else if ((checkArray >= 0) && (dialingCode === 234)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  else if (phone.length == 14) {
+    //get mobile number prefix from entered value
+    mobilePrefix = Number(phoneInputValue.slice(4,7));
+
+    //get dialling code from mobile number - +234
+    dialingCode = phoneInputValue.slice(0,4);
+
+    //check if prefix exists in mobile prefix array
+    checkArray = in_array(mobilePrefix, telcoPrefixes);
+
+    //if prefix not found in array
+    if(checkArray === false){
+      return false;
+    //if found in array
+    } else if ((checkArray >= 0) && (dialingCode === "+234")){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  else {
+    return false;
+  } 
 }
 
 //VALIDATE SIGNUP
